9bcdfdea3c1a279c4df6649c2eabc4183ac39ac5
Miguel Angel Rodriguez Candelario <themike54@outlook.com>
2026-09-01 11:04:00 -0600

    feat: app  Hola mundo en xml

---
+++ hola_mundo_xml/app/src/main/java/com/example/hola_mundo_xml/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.hola_mundo_xml
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hola_mundo_xml.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aplica márgenes para las barras del sistema (status bar / navigation bar)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Si deseas acceder a tus TextViews desde código en el futuro, puedes hacerlo así:
+        // binding.txtNombre.text = "Tu Nombre Real"
+    }
+}
